ca01b075594adfdecdc8e03ab67252191446f84c
André Burger <andreburger.furg@gmail.com>
2025-08-28 22:54:04 -0300

    refactor: Reorganiza schemas e melhora visualização do banco
    - Reorganiza schemas SQL em arquivos separados por domínio
    - Adiciona pacote tabulate para melhor visualização dos dados
    - Implementa nova classe DatabaseInitializer para gerenciar banco
    - Atualiza .gitignore com padrões mais abrangentes

--- scripts/init_db/classes/schema_class_equipment.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS class_equipment;
-
-CREATE TABLE class_equipment (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    class_id INTEGER NOT NULL,
-    item_option TEXT,
-    FOREIGN KEY (class_id) REFERENCES classes(id)
-);
--- scripts/init_db/classes/schema_classes.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS classes;
-
-CREATE TABLE classes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    description TEXT,
-    min_attribute TEXT,
-    hit_dice TEXT
-);
